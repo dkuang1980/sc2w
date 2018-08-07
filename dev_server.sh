02a62a9fa6d447bc9cde104952a91c1f15056a6d
Da Kuang <dak@surveymonkey.com>
2018-08-07 17:27:51 -0400

    try to fix shell formats

--- dev_server.sh
+++ dev_server.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
-python manage.py migrate --settings=sc2w.conf.dev
-
-python manage.py runserver --settings=sc2w.conf.dev 0.0.0.0:8000
+python manage.py migrate --settings=sc2w.conf.dev&&python manage.py runserver --settings=sc2w.conf.dev 0.0.0.0:8000
